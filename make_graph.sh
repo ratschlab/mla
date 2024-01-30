253c73f08a1ce185a28d69c3da0ab8a6db9a87d9
Harun Mustafa <bilgisayaradami@gmail.com>
2024-01-30 02:43:15 +0100

    script for making joint graph

---
+++ make_graph.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GRAPH=fungi
+NTHREADS=10
+
+# build metagraph index from reads
+ls nobackup/assemblies/*.path_cover.fasta.gz | ./metagraph build -k 31 --fwd-and-reverse -o $GRAPH -p $NTHREADS
+
+mkdir -p nobackup/columns
+mkdir -p tmp
+ls nobackup/assemblies/*.path_cover.fasta.gz | ./metagraph annotate -i $GRAPH.dbg -o nobackup/columns --anno-filename --separately --coordinates --anno-seq-ends -p $NTHREADS
+for i in 0 1 2; do
+    ls nobackup/columns/*.annodbg | ./metagraph transform_anno -p $NTHREADS --anno-type row_diff --coordinates -o out --row-diff-stage $i -i $GRAPH.dbg --disk-swap tmp
+done
+ls nobackup/columns/*.annodbg | ./metagraph transform_anno -p $NTHREADS --anno-type column --coordinates -o fungi
+ls *.column.annodbg | ./metagraph transform_anno -p $NTHREADS -i $GRAPH.dbg --anno-type row_diff_brwt_coord --greedy -o $GRAPH
+ls nobackup/columns/*.annodbg | ./metagraph transform_anno --sketch-precision 0.05 -o $GRAPH
