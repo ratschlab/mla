02a8d647bb21b979ce7d81b12eb520ed7f4d12c2
Harun Mustafa <hmusta@users.noreply.github.com>
2024-03-20 17:58:34 +0100

    Update map_query_slc.sh

--- map_query_slc.sh
+++ map_query_slc.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-CHAIN_XDROP=27
-
-PARAMS="align --align-alternative-alignments 5 --align-chains-per-char 0.01 --align-ignore-discarded-seeds -i fungi.dbg --align-min-exact-match 0.0 --align-rel-score-cutoff 0.0 --align-edit-distance --align-gap-open-penalty 1 --align-gap-extension-penalty 1 -v $1 --batch-size 10000"
+PARAMS="align -i fungi.dbg -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --align-edit-distance -v --align-profile SCA $1"
 
 mkdir -p alignments
 
 echo "With chaining"
-OUTPREFIX="alignments/$(basename $1).$2"
-/usr/bin/time -v ./metagraph $PARAMS --align-xdrop $CHAIN_XDROP -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --path-cover >$OUTPREFIX.tsv 2>$OUTPREFIX.log
+OUTPREFIX="alignments/$(basename $1).0.01"
+/usr/bin/time -v ./metagraph $PARAMS >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
