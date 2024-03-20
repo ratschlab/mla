a280fed4c82d8704ee0df0dab9b3ac5ddc05413b
Harun Mustafa <hmusta@users.noreply.github.com>
2024-03-20 17:57:34 +0100

    Update map_query_mla.sh

--- map_query_mla.sh
+++ map_query_mla.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-CHAIN_XDROP=27
-
-PARAMS="align --align-alternative-alignments 5 --align-chains-per-char 0.01 --align-ignore-discarded-seeds -i fungi.dbg --align-min-exact-match 0.0 --align-rel-score-cutoff 0.0 --align-edit-distance --align-gap-open-penalty 1 --align-gap-extension-penalty 1 -v $1 --batch-size 10000"
+PARAMS="align -i fungi.dbg -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --align-edit-distance -v --align-profile mla $1"
 
 mkdir -p alignments
 
 echo "With MLA chaining"
 OUTPREFIX="alignments/$(basename $1).$2.mla"
-/usr/bin/time -v ./metagraph $PARAMS --align-xdrop $CHAIN_XDROP -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --path-cover --align-post-chain >$OUTPREFIX.tsv 2>$OUTPREFIX.log
+/usr/bin/time -v ./metagraph $PARAMS >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
 OUTPREFIX="alignments/$(basename $1).$2.mlafixha"
-/usr/bin/time -v ./metagraph $PARAMS --align-xdrop $CHAIN_XDROP -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --path-cover --align-post-chain --align-fixed-haplotype 10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
+/usr/bin/time -v ./metagraph $PARAMS --align-fixed-haplotype 10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
 OUTPREFIX="alignments/$(basename $1).$2.mlanonlc"
-/usr/bin/time -v ./metagraph $PARAMS --align-xdrop $CHAIN_XDROP -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.column.annodbg --path-cover --align-post-chain --align-fixed-haplotype -10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
+/usr/bin/time -v ./metagraph $PARAMS --align-fixed-haplotype -10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
