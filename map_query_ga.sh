47d147dbf84c47b0b59bcbb78c5ec087f2c4fa59
Harun Mustafa <hmusta@users.noreply.github.com>
2024-03-20 17:22:25 +0100

    Update map_query_ga.sh

--- map_query_ga.sh
+++ map_query_ga.sh
@@ -5,5 +5,5 @@ PARAMS="-g fungi.gfa -a $OUTPREFIX.gaf -x dbg -f $1"
 
 mkdir -p ga_jointgraph
 
-/usr/bin/time -v ./GraphAligner $PARAMS >$OUTPREFIX.log 2>&1
+/usr/bin/time -v GraphAligner $PARAMS >$OUTPREFIX.log 2>&1
 ./classify_ga.py $OUTPREFIX.gaf $1 >$OUTPREFIX.gaf.wgsunifrac.sweep.tsv 2>$OUTPREFIX.gaf.wgsunifrac.sweep.log
