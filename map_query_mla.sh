2ceaf5cf04775b42201759b3526ab9ada0ec750b
Harun Mustafa <hmusta@users.noreply.github.com>
2024-03-20 17:59:29 +0100

    Update map_query_mla.sh

--- map_query_mla.sh
+++ map_query_mla.sh
@@ -5,15 +5,15 @@ PARAMS="align -i fungi.dbg -a fungi.row_diff_brwt_coord.annodbg -a fungi.seq.col
 mkdir -p alignments
 
 echo "With MLA chaining"
-OUTPREFIX="alignments/$(basename $1).$2.mla"
+OUTPREFIX="alignments/$(basename $1).0.01.mla"
 /usr/bin/time -v ./metagraph $PARAMS >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
-OUTPREFIX="alignments/$(basename $1).$2.mlafixha"
+OUTPREFIX="alignments/$(basename $1).0.01.mlafixha"
 /usr/bin/time -v ./metagraph $PARAMS --align-fixed-haplotype 10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
-OUTPREFIX="alignments/$(basename $1).$2.mlanonlc"
+OUTPREFIX="alignments/$(basename $1).0.01.mlanonlc"
 /usr/bin/time -v ./metagraph $PARAMS --align-fixed-haplotype -10 >$OUTPREFIX.tsv 2>$OUTPREFIX.log
 ./classify_mg.py $OUTPREFIX.tsv $1 >$OUTPREFIX.tsv.wgsunifrac.sweep.tsv 2>$OUTPREFIX.tsv.wgsunifrac.sweep.log
 
